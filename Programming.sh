43e059921d686c1f1a2a831e92e13c6e444d8ba7
Suresh Guguloth <sureshnaik.guguloth@gmail.com>
2017-02-01 16:38:27 +0530

    Update Programming.sh

--- Programming.sh
+++ Programming.sh
@@ -1,5 +1,5 @@
 
 sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt-get install oracle-java8-installer -y
+sudo apt-get install oracle-java8-installer r-base rar unrar
 sudo apt-get update
 sudo apt-get upgrade
