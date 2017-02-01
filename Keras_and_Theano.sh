e92ddae76f33c7326c0bc27f8ed15fc6cf82e371
Suresh Guguloth <sureshnaik.guguloth@gmail.com>
2017-01-31 23:16:20 -0800

    Create Keras_and_Theano.sh

---
+++ Keras_and_Theano.sh
@@ -0,0 +1,9 @@
+echo "Keras Installation is started"
+pip install --upgrade --no-deps git+git://github.com/fchollet/keras.git --user
+echo "Keras Installation is Finished"
+
+echo "Theano Installation is started"
+pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git --user
+echo "Theano Installation is Finished"
+
+
